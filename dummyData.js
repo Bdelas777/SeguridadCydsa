b60ac50340b9c9975a081a55b311fe0ea8cfcf0c
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2025-02-26 11:07:28 -0600

    Update inspectores

--- dummyData.js
+++ dummyData.js
@@ -1,6 +1,37 @@
 const dummyData = [
   {
     planta: "Q002-01",
+    formatos: [
+      {
+        codigo: "FSP-001",
+        nombre: "Formulario de planeación",
+        norma: "base 1234",
+        revision: 0,
+        preguntas: [
+          "Si esta aplicando el informe",
+          "Ejemplo"
+        ]  
+      },
+      {
+        codigo: "FSP-002",
+        nombre: "Formulario de ejecución",
+        norma: "base 1344",
+        revision: 2,
+        preguntas: [
+          "Ejemplo 2 ",
+          "Ejemplo 3"
+        ]  
+      },
+    ],
+    rubros: [
+      {
+        nombre: "Investigación",
+        porcentaje: 10,
+        minimo: 1,
+        obligatorio: true,
+        fecha: "24/09/2025"
+      }
+    ],
     superintendentes: [
       {
         nombre: "Juan Pérez",
@@ -132,6 +163,79 @@ const dummyData = [
   },
   {
     planta: "Q010-01",
+    formatos: [
+      {
+        codigo: "FSP-003",
+        nombre: "Formulario de planeación",
+        revision: 1,
+        norma: "base 1234",
+        preguntas: [
+          "Si esta aplicando el informe",
+          "Ejemplo"
+        ]  
+      },
+      {
+        codigo: "FSP-004",
+        nombre: "Formulario de ejecución",
+        revision: 0,
+        norma: "base 1344",
+        preguntas: [
+          "Ejemplo 2 ",
+          "Ejemplo 3"
+        ]  
+      },
+    ],
+    rubros: [
+      {
+        nombre: "Inducción de Seguridad al Personal de Nuevo Ingreso",
+        porcentaje: 10,
+        minimo: 1,
+        obligatorio: true,
+        fecha: "24/09/2025"
+      },
+      {
+        nombre: "Entrenamiento al Personal Cambiado de Puesto EDITADO",
+        porcentaje: 10,
+        minimo: 1,
+        obligatorio: false,
+        fecha: "24/09/2025"
+      },
+      {
+        nombre: "Investigación de Accidentes",
+        porcentaje: 10,
+        minimo: 2,
+        obligatorio: true,
+        fecha: "24/09/2025"
+      },
+      {
+        nombre: "Inspecciones Planeadas",
+        porcentaje: 12.5,
+        minimo: 1,
+        obligatorio: false,
+        fecha: "24/09/2025"
+      },
+      {
+        nombre: "Identificación de tareas Críticas",
+        porcentaje: 12.5,
+        minimo: 1,
+        obligatorio: false,
+        fecha: "24/09/2025"
+      },
+      {
+        nombre: "Observación de seguridad",
+        porcentaje: 12.5,
+        minimo: 1,
+        obligatorio: false,
+        fecha: "24/09/2025"
+      },
+      {
+        nombre: "Contactos Personales",
+        porcentaje: 12.5,
+        minimo: 1,
+        obligatorio: false,
+        fecha: "24/09/2025"
+      },
+    ],
     superintendentes: [
       {
         nombre: "María González",
@@ -203,6 +307,21 @@ const dummyData = [
   },
   {
     planta: "Q010-02",
+    formatos: [
+      {
+        codigo: "FSP-005",
+        nombre: "Formulario de planeación",
+        norma: "base 1234",
+        revision: 3,
+        preguntas: [
+          "Si esta aplicando el informe",
+          "Ejemplo"
+        ]  
+      },
+    ],
+    rubros: [
+
+    ],
     superintendentes: [
       {
         nombre: "Carlos Rodríguez",
@@ -244,6 +363,27 @@ const dummyData = [
   },
   {
     planta: "Q010-03",
+    formatos: [
+      {
+        codigo: "FSP-006",
+        nombre: "Formulario de planeación",
+        revision: 1,
+        norma: "base 1234",
+        preguntas: [
+          "Si esta aplicando el informe",
+          "Ejemplo"
+        ]  
+      },
+    ],
+    rubros: [
+      {
+        nombre: "Investigación de prueba",
+        porcentaje: 10,
+        minimo: 1,
+        obligatorio: true,
+        fecha: "24/09/2025"
+      }
+    ],
     superintendentes: [
       {
         nombre: "Francisco Mendoza",
@@ -273,7 +413,7 @@ const dummyData = [
                   },
                   {
                     nombre: "Wilson Fuentes",
-                    inspectores: ["Jesus brown"]
+                    inspectores: ["Jesus Brown"]
                   }
                 ]
               }
@@ -316,6 +456,26 @@ const dummyData = [
   },
   {
     planta: "Q083-01",
+    formatos: [
+      {
+        codigo: "FSP-007",
+        nombre: "Formulario de planeación",
+        revision: 0,
+        norma: "base 1234",
+        preguntas: [
+          "Si esta aplicando el informe",
+          "Ejemplo"
+        ]  
+      },
+    ], rubros: [
+      {
+        nombre: "<pruebas",
+        porcentaje: 10,
+        minimo: 1,
+        obligatorio: true,
+        fecha: "24/09/2025"
+      }
+    ],
     superintendentes: [
       {
         nombre: "Alicia Ramírez",
@@ -363,3 +523,5 @@ const dummyData = [
     "Luis Martínez", "Laura Sánchez", "Roberto González", "Patricia Díaz",
     "Miguel Hernández", "Claudia Torres", "Fernando Ramírez", "Silvia Castro"
   ];
+
+  
